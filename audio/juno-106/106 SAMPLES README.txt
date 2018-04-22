904f09db707932621e3dee9707a0606b73a513b6
Adam Prince <adam@adamprince.us>
2018-04-21 20:12:21 -0700

    lots of changes
    notably, integrated howl.js and cleaned up the code a hell of a lot

---
+++ audio/juno-106/106 SAMPLES README.txt
@@ -0,0 +1,17 @@
+Hello
+
+Thank you for downloading the Legowelt Roland JUNO 106 sample kit!
+250 stereo WAV samples in categories such as Bass, Drums. FX. Laser , Pad, Sequence, Strings, Synth etc.
+
+This sample kit is free, you can do whatever you want it
+If u like it or make a multimillion $$$ super hit with it you can always paypall a donation 
+to wolfers@xs4all.nl. 
+
+Keep an eye out for some other Sample kits from some synths in my studio in the future…
+
+Regards and have fun with it!
+
+D.
+
+http://www.legowelt.com
+©2012 Pacific Micro International Software
--- js/main.js
+++ js/main.js
@@ -1,17 +1,138 @@
-  function playSound(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-    if (!audio) return;
-    key.classList.add('playing');
-    audio.currentTime = 0;
-    audio.play();
+var keyData = {
+  q: {
+    sound: new Howl({
+      src: ['audio/606/q.wav']
+    })
+  },
+  w: {
+    sound: new Howl({
+      src: ['audio/606/w.wav']
+    })
+  },
+  e: {
+    sound: new Howl({
+      src: ['audio/606/e.wav']
+    })
+  },
+  r: {
+    sound: new Howl({
+      src: ['audio/606/r.wav']
+    })
+  },
+  t: {
+    sound: new Howl({
+      src: ['audio/606/t.wav']
+    })
+  },
+  y: {
+    sound: new Howl({
+      src: ['audio/606/y.wav']
+    })
+  },
+  u: {
+    sound: new Howl({
+      src: ['audio/606/u.wav']
+    })
+  },
+  i: {
+    sound: new Howl({
+      src: ['audio/606/i.wav']
+    })
+  },
+  o: {
+    sound: new Howl({
+      src: ['audio/606/o.wav']
+    })
+  },
+  p: {
+    sound: new Howl({
+      src: ['audio/606/p.wav']
+    })
+  },
+  a: {
+    sound: new Howl({
+      src: ['audio/606/a.wav']
+    })
+  },
+  s: {
+    sound: new Howl({
+      src: ['audio/606/s.wav']
+    })
+  },
+    d: {
+    sound: new Howl({
+      src: ['audio/606/d.wav']
+    })
+  },
+  f: {
+    sound: new Howl({
+      src: ['audio/606/f.wav']
+    })
+  },
+  g: {
+    sound: new Howl({
+      src: ['audio/606/g.wav']
+    })
+  },
+  h: {
+    sound: new Howl({
+      src: ['audio/606/h.wav']
+    })
+  },
+  j: {
+    sound: new Howl({
+      src: ['audio/606/j.wav']
+    })
+  },
+  k: {
+    sound: new Howl({
+      src: ['audio/606/k.wav']
+    })
+  },
+  l: {
+    sound: new Howl({
+      src: ['audio/606/l.wav']
+    })
+  },
+  z: {
+    sound: new Howl({
+      src: ['audio/606/z.wav']
+    })
+  },
+  x: {
+    sound: new Howl({
+      src: ['audio/606/x.wav']
+    })
+  },
+  c: {
+    sound: new Howl({
+      src: ['audio/606/c.wav']
+    })
+  },
+  v: {
+    sound: new Howl({
+      src: ['audio/606/v.wav']
+    })
+  },
+  b: {
+    sound: new Howl({
+      src: ['audio/606/b.wav']
+    })
+  },
+  n: {
+    sound: new Howl({
+      src: ['audio/606/n.wav']
+    })
+  },
+  m: {
+    sound: new Howl({
+      src: ['audio/606/m.wav']
+    })
   }
+};
 
-  function removeTransition(e) {
-    if (e.propertyName !== 'transform') return;
-    this.classList.remove('playing');
+document.addEventListener('keydown', function (event) {
+  if(event.key){
+    keyData[event.key].sound.play();
   }
-
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-window.addEventListener('keydown', playSound);
+});
